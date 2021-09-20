3f34877b12e79a6a7501246c62e20ca12860ccbd
Vitalii Tsyoma <vitaliytsyoma@icloud.com>
2021-09-20 15:19:15 +0300

    test

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
 
+
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
